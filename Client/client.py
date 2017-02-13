f6b103902b22eded4507b882edceb4e16e56937d
Josh Smith <jsmith@loop-depaulsecure-234-186.depaulsecure-student.depaul.edu>
2017-02-13 14:56:11 -0600

    Adding untested/incomplete SSL integration

--- Client/client.py
+++ Client/client.py
@@ -1,26 +1,69 @@
 import sys
 import socket
 
+import ssl
+import hashlib
+
 
 class Client:
     def __init__(self):
-        host = 'localhost'
-        port = 8001
+        self.connected = False
 
-        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.sock.connect((host,port))
-        print("Success")
+        # TODO: NEED TO ADD CODE TO IMPORT A PUB KEY (or cert) WHICH WE WILL PUT IN THE CLIENT FILES AHEAD OF TIME!
+
+
+        #host = 'localhost'
+        #port = 8001
+
+        #self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        #self.sock.connect((host,port))
+        print("Initialized")
         #message = "query:" + sys.stdin.readline()
         #self.sock.send(message.encode())
 
+    def connect(self, host='localhost', port=8001, use_ssl=True):
+        #parameter: host -> The desired host for the new connection.
+        #parameter: port -> The desired port for the new connection.
+        #parameter: use_ssl -> Can be set to False to disable SSL for the client connecting
+
+        # Code to get the server's cert
+        #cert = conn.getpeercert()
+        #If it's self signed, I dont think we need a CA cert to verify?
+
+        if not self.connected:
+            self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            self.secureSock = ssl.wrap_socket(self.sock, server_side = False)
+            self.secureSock.connect((host, port))
+            self.connected = True
+            print("Secure connection successful!")
+        else:
+            print("Client already connected to a server! Disconnect first.")
+
+    def disconnect(self):
+        if self.connected:
+            self.secureSock.shutdown()
+            self.secureSock.close()
+            self.connected = False
+            print("Disconnection successful!")
+        else:
+            print("Nothing to disconnect!")
+
     def login(self, username, password):
         if username and password:
+            # Add password hashing code in here (use username as salt?)
+            # Note that sha3_512 requires 3.6. sha512 is a less secure option
+            # to maintain compatibility with older platforms
+            hashedPass = hashlib.sha3_512(password).hexdigest()
             return 1
         else:
             return 0
 
     def register(self, username, password):
         if username and password:
+            # Add password hashing code in here (use username as salt?)
+            # Note that sha3_512 requires 3.6. sha512 is a less secure option
+            # to maintain compatibility with older platforms
+            hashedPass = hashlib.sha3_512(password).hexdigest()
             return 1
         else:
             return 0
--- Server/server.py
+++ Server/server.py
@@ -3,6 +3,8 @@ import socket
 from request_handler import RequestHandler
 from server_globals import connections
 
+import ssl
+import OpenSSL
 
 def create_socket(port, host='localhost'):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,6 +13,56 @@ def create_socket(port, host='localhost'):
     print("Server started on {}:{}".format(host, port))
     return server
 
+def create_secure_socket(port, host='localhost', cert_dir):
+    context = ssl.SSLContext() # Defaults to SSL/TLS support
+    context.verify_mode = ssl.CERT_OPTIONAL # ssl.CERT_REQUIRED is more secure
+    context.check_hostname = False # Hostname verification on certs (Dont want for now)
+    context.load_default_certs(purpose=Purpose.CLIENT_AUTH) # Load the public CA certs for the server socket (need CLIENT_AUTH param)
+    context.load_cert_chain(certfilePath, keyfile=keyfilePath) # TODO FIX THE certfile path!!!!!!!!!!
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    secureSocket = context.wrap_socket(server, server_side = True)
+    secureSocket.bind((host, port))
+    print("SSL Server started on {}:{}".format(host, port))
+    return secureSocket
+
+def generate_server_self_cert(cert_dir):
+    #parameter: cert_dir -> The directory to store the certificates in.
+
+    # Add code to call OpenSSL to generate a certificate for the server to use
+    # Might require an import of PyOpenSSL (OpenSSL)
+
+    CERT_FILE = "KnowledgeManagement.crt"
+    KEY_FILE = "KnowledgeManagement.key"
+
+    if not exists(join(cert_dir, CERT_FILE)) \
+            or not exists(join(cert_dir, KEY_FILE)):
+            # create a key pair
+        publicKey = crypto.PKey()
+        publicKey.generate_key(crypto.TYPE_RSA, 1024)
+
+        # create a self-signed cert
+        cert = crypto.X509()
+        cert.get_subject().C = "US"
+        cert.get_subject().ST = "Illinois"
+        cert.get_subject().L = "Chicago"
+        cert.get_subject().O = "CSC 376 - Distributed Systems"
+        cert.get_subject().OU = "Knowledge Management Group"
+        cert.get_subject().CN = socket.gethostname()
+        cert.set_serial_number(1000)
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(10*365*24*60*60)
+        cert.set_issuer(cert.get_subject())
+        cert.set_pubkey(publicKey)
+        cert.sign(publicKey, 'sha1') # SHA1 has known theoretical attack to produce collisions!
+
+        open(join(cert_dir, CERT_FILE), "wt").write(
+            crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+        open(join(cert_dir, KEY_FILE), "wt").write(
+            crypto.dump_privatekey(crypto.FILETYPE_PEM, publicKey))
+
+    else:
+        print("Certificate/Key already exist! A new pair will not be generated.")
 
 def server_loop(server):
     while True:
