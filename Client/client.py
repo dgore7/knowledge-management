2bd6e31fadc0f1e7aa9a89cef6925367ac094ac1
Jasmine Farley <jasminefarley@Jasmines-Air.hsd1.il.comcast.net>
2017-02-26 20:45:16 -0600

    High level design

--- Client/client.py
+++ Client/client.py
@@ -4,30 +4,71 @@ import os
 import time
 
 import codecs
+import ssl
+
+
+# import OpenSSL
 
 
 class Client:
     def __init__(self):
-        print("Client Created")
+        self.connected = False
 
-    def connect(self):
-        host = 'localhost'
-        port = 8001
+        # TODO: NEED TO ADD CODE TO IMPORT A PUB KEY (or cert) WHICH WE WILL PUT IN THE CLIENT FILES AHEAD OF TIME!
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((host, port))
-        print("Success")
+        # self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        # self.sock.connect((host,port))
+        # TODO: ALTER ROUTINES TO INSTEAD CONNECT USING A CONNECT BUTTON. THEN CERTS CAN BE SET BEFOREHAND!
+        self.connect()
+        print("Initialized")
+        # message = "query:" + sys.stdin.readline()
+        # self.sock.send(message.encode())
 
-        return sock
+    def connect(self, cert_file_path='/Users/jasminefarley/Documents/CSC376/keyfiles/KnowledgeManagement.crt',
+                host='localhost', port=8001):
+        # parameter: host -> The desired host for the new connection.
+        # parameter: port -> The desired port for the new connection.
+        # parameter: use_ssl -> Can be set to False to disable SSL for the client connecting
+
+        # Code to get the server's cert
+        # We need this to verify it (the cert is its own root)
+        # cert = conn.getpeercert()
+
+        if not self.connected:
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)  # Defaults to SSL/TLS support
+            context.verify_mode = ssl.CERT_REQUIRED  # ssl.CERT_REQUIRED is more secure
+            context.check_hostname = True  # Hostname verification on certs (Dont want for now)
+            # TODO: Alter the SSL location
+            context.load_verify_locations(cafile=cert_file_path)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock = context.wrap_socket(self.sock, server_side=False,
+                                            server_hostname=socket.gethostname())
+
+            # TODO: Add a try for the line below to handle certificate mismatch errors and others
+            self.sock.connect((host, port))
+            self.connected = True
+            print("Secure connection successful!")
+        else:
+            print("Client already connected to a server! Disconnect first.")
+
+    def disconnect(self):
+        if self.connected:
+            self.sock.shutdown(socket.SHUT_RDWR)
+            self.sock.close()
+            self.connected = False
+            print("Disconnection successful!")
+        else:
+            print("Nothing to disconnect!")
 
     def login(self, username, password):
-        connection = self.connect()
+        connection = self.sock
         connection.send("login".encode())
         print("Hello")
         status_code = connection.recv(2)
         print("MSG Replayed")
+        decoded_status_code = status_code.decode()
 
-        if status_code.decode != "OK":
+        if decoded_status_code != "OK":
             print("Failled")
             return
 
@@ -36,18 +77,22 @@ class Client:
         print(login_info)
 
         connection.send(login_info.encode())
-        connection.close()
-        if username and password:
+        # self.sock.send(login_info.encode())
+        # connection.close()
+        server_response = connection.recv(19).decode().split("|")  # "login_response|bad" or "login_response|good"
+        if server_response[0] == "login_response" and server_response[1] == "good":
             return 1
         else:
             return 0
 
     def register(self, username, password):
-        sock = self.connect()
-        sock.send("register".encode())
+        if self.connected == False:
+            return 0  # Maybe change to a unique code stating we are not connected?
+        connection = self.sock
+        connection.send("register".encode())
 
         register_info = username + ":" + password
-        sock.send(register_info.encode())
+        connection.send(register_info.encode())
 
         if username and password:
             return 1
@@ -55,7 +100,7 @@ class Client:
             return 0
 
     def upload(self, filename, category, keywords):
-        connection = self.connect()
+        connection = self.sock
 
         connection.send("upload".encode())
 
@@ -84,28 +129,29 @@ class Client:
 
         file.close()
         print("Closing file")
-        connection.close()
+        # connection.close()
 
     def retrieve(self, filename):
-        sock = self.connect()
-        sock.send("retrieve".encode())
+        connection = self.sock
+        connection.send("retrieve".encode())
 
-        sock.send(filename.encode())
+        connection.send(filename.encode())
         print(filename)
-        sock.close()
+        # sock.close()
 
     def search(self, filename):
-        sock = self.connect()
-        sock.send("search".encode())
+        connection = self.sock
+        connection.send("search".encode())
 
         # Maybe can use query statement here
-        sock.send(filename.encode())
+        connection.send(filename.encode())
         print(filename)
-        sock.close()
+        # sock.close()
 
     def delete(self, filename):
-        sock = self.connect()
-        sock.send("delete".encode())
-        sock.send(filename.encode())
+        connection = self.sock
+        connection.send("delete".encode())
+        connection.send(filename.encode())
         print(filename)
-        sock.close()
+        # sock.close()
+
