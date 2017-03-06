19a1308b9f102be98d0980a609055b35152cf165
Jessica Hua <jessicahua95@gmail.com>
2017-03-05 23:51:14 -0600

    Working on file decryption
    Runs but does not decrypt right

--- Client/fileEncryption.py
+++ Client/fileEncryption.py
@@ -4,6 +4,9 @@ import os
 import random
 import struct
 from Crypto.Cipher import AES
+import binascii
+from base64 import b64encode 
+
 
 class fileEncoding: 
 
@@ -13,23 +16,29 @@ class fileEncoding:
 	def getEncryptedFile(self):
 		return self.filename
 
+	def getKey(self):
+		return self.key
+
 	def encryptFile(self, filename): 
 		blocksize = 64 * 1024
-		key = os.urandom(16)
-
+		self.key = os.urandom(16)
 		encryptedFile = filename + '.enc'
 
 		iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
-		cipher = AES.new(key, AES.MODE_CBC, iv)
+		cipher = AES.new('This is my key12', AES.MODE_CBC, iv)
+		# self.key = binascii.hexlify(self.key)
+		self.key = b64encode(self.key).decode('utf-8')
 		fileSize = os.path.getsize(filename)
 
-		inputFile = open(filename, 'r')
-		outputFile = open(encryptedFile, 'w')
+		inputFile = open(filename, 'rb')
+		outputFile = open(encryptedFile, 'wb')
 		outputFile.write(struct.pack('<Q', fileSize))
 		outputFile.write(iv)
 
 		while len(inputFile.read(blocksize)) > 0:
 			block = inputFile.read(blocksize)
+			if len(block) == 0:
+				break
 	        if len(block) % 16 != 0:
 	            block += ' ' * (16 - len(block) % 16)
 
@@ -37,4 +46,5 @@ class fileEncoding:
 
 
 a = fileEncoding("encrypt.txt")
+print(a.getKey())
 
